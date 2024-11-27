d1946d61a21ecf5ab10c02823b2b53aeab86f2c9
datanico <nicolascorbi2002@gmail.com>
2024-11-27 11:10:24 +0100

    hshf

--- hugginface/main.py
+++ hugginface/main.py
@@ -7,7 +7,7 @@ KEY = os.getenv("KEY")
 API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
 headers = {"Authorization": f"Bearer {KEY}"}
 payload = {
-    "inputs": "Today is a great day",
+    "inputs": "Hola mundo",
 }
 
 response = requests.post(API_URL, headers=headers, json=payload)
